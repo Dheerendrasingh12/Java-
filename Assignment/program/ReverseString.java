fb581bb4467c59816da246c387a993229b2d10a8
Dheerendrasingh12 <34004122+Dheerendrasingh12@users.noreply.github.com>
2020-01-01 10:28:27 +0530

    Update ReverseString.java

--- Assignment/program/ReverseString.java
+++ Assignment/program/ReverseString.java
@@ -2,7 +2,7 @@ class ReverseString
 {
 	public static void main(String ...arg)
 	{
-		String str="hello dheerendra singh chouhan is here";
+		String str="hello dheerendra singh dhakad is here";
 		char[] ch=str.toCharArray();
 		char[] newch=new char[ch.length];
 		for(int i=ch.length-1,j=0 ;i>=0 ; i--,j++)
